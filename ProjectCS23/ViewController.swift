c626038f427c8822fed3e88275510de226301409
Morimel <melsovisa77@gmail.com>
2024-12-04 00:10:17 +0600

    Initial Commit

---
+++ ProjectCS23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectCS23
+//
+//  Created by Isa Melsov on 4/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ProjectCS23Tests/ProjectCS23Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectCS23Tests.swift
+//  ProjectCS23Tests
+//
+//  Created by Isa Melsov on 4/12/24.
+//
+
+import Testing
+@testable import ProjectCS23
+
+struct ProjectCS23Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
